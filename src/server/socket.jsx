afae67f6fc84a3e7ec96ef84af0f05ba1dee9c02
ethzhou <ethzhou@gmail.com>
2024-01-28 08:17:39 -0800

    one more initial commit

--- src/server/socket.jsx
+++ src/server/socket.jsx
@@ -1,6 +1,7 @@
 import io from "socket.io-client";
 
 // export const socket = io("backend url here!!!11");
-// export const socket = io("ws://localhost:8080");
+export const socket = io("ws://localhost:8080");
 // export const socket = io("https://ytess-a0d4f.web.app");
-export const socket = io("https://ytess-backend.onrender.com");
+// export const socket = io("https://ytess-backend.onrender.com");
+// export const socket = io("ws://localhost:8081");
